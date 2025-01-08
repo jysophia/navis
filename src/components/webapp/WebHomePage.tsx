0b4a8a400f2ab688fd0bfe5be4c06c9a43184b57
mcctm <mirandachanct@gmail.com>
2025-01-07 21:50:44 -0800

    add placeholder content for web app home page and nav bar, change icon

--- src/components/webapp/WebHomePage.tsx
+++ src/components/webapp/WebHomePage.tsx
@@ -1,7 +1,28 @@
+import Navbar from './Navbar';
+
 export default function WebHomePage(){
     return (
-        <div>
-            <h1>WebApp page</h1>
+        <div className="flex-container">
+            <Navbar />
+            <div className="flex-child">
+                <img src='/public/navisLogo.png' />
+                <h1>Hey Navis,</h1>
+                <h2>You added 1 new scholarship! Go check it out</h2>
+                <h3>Applications in Progress</h3>
+                <ul>
+                    <li>Pacific Oceans Award</li>
+                    <li>The Dead Sea Nominations</li>
+                    <li>Ice Caps Beneficial</li>
+                </ul>
+                <h3>Featured News</h3>
+                <ul>
+                    <li>This is a news about a scholarship.</li>
+                </ul>
+            </div>
+            
+            <div className="flex-child">
+                <h2>My calendar</h2>
+            </div>
         </div>
     )
 }
