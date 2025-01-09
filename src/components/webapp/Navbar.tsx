941bed51328b4ebc5e2bb627fdf74d5cb01733ba
mcctm <mirandachanct@gmail.com>
2025-01-08 22:07:16 -0800

    add styling for navbar

--- src/components/webapp/Navbar.tsx
+++ src/components/webapp/Navbar.tsx
@@ -1,7 +1,11 @@
 export default function Navbar(){
     return (
         <div className="navBarContainer">
-            <span className="navBarLogo">Navis</span>
+            <span className="navBarLogo">navis</span>
+            <i className="bi bi-person-circle navBarOption"></i>
+            <i className="bi bi-bell navBarOption"></i>
+            <i className="bi bi-bookmark navBarOption"></i>
+            <i className="bi bi-list-task navBarOption"></i>
         </div>
     )
 }
